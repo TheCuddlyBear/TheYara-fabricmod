252f165b37b76d49fec7c518038eb2092d261394
TheCuddlyBear <basvthazeveld@outlook.com>
2021-08-19 00:42:10 +0200

    Implemented shroomie model

--- src/main/java/me/thecuddlybear/theyara/entity/ShroomieEntityModel.java
+++ src/main/java/me/thecuddlybear/theyara/entity/ShroomieEntityModel.java
@@ -9,28 +9,50 @@ import net.minecraft.client.util.math.MatrixStack;
 
 public class ShroomieEntityModel extends EntityModel<ShroomieEntity> {
 
-    private final ModelPart base;
+    private final ModelPart bb_main;
+    private final ModelPart cube_r1;
+    private final ModelPart cube_r2;
 
-    public ShroomieEntityModel(ModelPart modelPart){
-        this.base = modelPart.getChild(EntityModelPartNames.CUBE);
+    public ShroomieEntityModel(ModelPart root) {
+        //  TODO: add bone fields here!
+        this.bb_main = root.getChild("bb_main");
+        this.cube_r1 = bb_main.getChild("cube_r1");
+        this.cube_r2 = bb_main.getChild("cube_r2");
     }
 
-    public static TexturedModelData getTexturedModelData() {
+    public static ModelData getModelData(){
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild(EntityModelPartNames.CUBE, ModelPartBuilder.create().uv(0, 0).cuboid(-6F, 12F, -6F, 12F, 12F, 12F), ModelTransform.pivot(0F, 0F, 0F));
-        return TexturedModelData.of(modelData, 64, 64);
+
+        ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -7.0F, -4.0F, 8.0F, 3.0F, 8.0F, new Dilation(0.0F)).mirrored(false)
+                .uv(0, 11).cuboid(-2.0F, -4.0F, -2.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)).mirrored(false)
+                .uv(4, 0).cuboid(1.0F, -1.0F, 2.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F)).mirrored(false)
+                .uv(4, 0).cuboid(-2.0F, -1.0F, 2.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(0.0F, 24.0F, 0.0F, 0.0F, 0.0F, 0.0F));
+
+        ModelPartData cube_r1 = bb_main.addChild("cube_r1", ModelPartBuilder.create().uv(0, 0).cuboid(0.0F, 5.0F, -1.0F, 1.0F, 3.0F, 2.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(0.0F, -8.0F, 0.0F, 0.0F, 0.0F, 0.3927F));
+
+        ModelPartData cube_r2 = bb_main.addChild("cube_r2", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, 5.0F, -1.0F, 1.0F, 3.0F, 2.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(0.0F, -8.0F, 0.0F, 0.0F, 0.0F, -0.3927F));
+        return modelData;
+    }
+
+
+    public static TexturedModelData getTexturedModelData() {
+        return TexturedModelData.of(getModelData(), 32, 32);
     }
 
     @Override
-    public void setAngles(ShroomieEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+    public void setAngles(ShroomieEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+        //previously the render function, render code was moved to a method below
     }
-
     @Override
-    public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
-        ImmutableList.of(this.base).forEach((modelRenderer) -> {
-            modelRenderer.render(matrices, vertices, light, overlay, red, green, blue, alpha);
-        });
+    public void render(MatrixStack matrixStack, VertexConsumer	buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+
+        bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
+    }
+    public void setRotationAngle(ModelPart bone, float x, float y, float z) {
+        bone.pitch = x;
+        bone.yaw = y;
+        bone.roll = z;
     }
 
 }
